0c83440858a3cbd7e9fca0469fedb1d7f8128a15
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-03-26 00:28:04 +0200

    finish_hw

---
+++ src/components/Statistics/StatisticsItem/StatisticsItem.jsx
@@ -0,0 +1,22 @@
+import css from './StatisticsItem.css';
+import { getRandomColor } from 'utils/getRandomColor';
+import PropTypes from 'prop-types';
+
+export const StatisticItem = ({ label, percentage }) => {
+  return (
+    <li
+      className="item"
+      style={{
+        backgroundColor: getRandomColor(),
+      }}
+    >
+      <span className="label">{label}</span>
+      <span className="percentage">{percentage}%</span>
+    </li>
+  );
+};
+
+StatisticItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
